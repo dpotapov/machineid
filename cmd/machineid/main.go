c54256ae3b8671f0e3279178269627ab3db45851
Daniel Potapov <potapovdaniel@gmail.com>
2023-05-13 04:23:40 +0000

    fix redundant newline and applied gofmt

--- cmd/machineid/main.go
+++ cmd/machineid/main.go
@@ -3,11 +3,13 @@
 // Usage: machineid [options]
 //
 // Options:
-//   --appid    <AppID>    Protect machine id by hashing it together with an app id.
+//
+//	--appid    <AppID>    Protect machine id by hashing it together with an app id.
 //
 // Try:
-//   machineid
-//   machineid --appid MyAppID
+//
+//	machineid
+//	machineid --appid MyAppID
 package main
 
 import (
@@ -26,8 +28,7 @@ Options:
 
 Try:
   machineid
-  machineid --appid MyAppID
-`
+  machineid --appid MyAppID`
 
 func usage() {
 	log.Fatalln(usageStr)
